4417f158b04060c5f8541da374d2e84174106224
llq <2807693233@qq.com>
2020-12-10 10:11:58 +0800

    测试第一次提交

---
+++ main/java/com/example1/demo/DemoApplication.java
@@ -0,0 +1,8 @@
+package com.example1.demo;
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("你好 世界");
+    }
+
+}
---
+++ test/java/com/example1/demo/DemoApplicationTests.java
@@ -0,0 +1,10 @@
+package java.com.example1.demo;
+
+class DemoApplicationTests {
+
+
+    public void contextLoads() {
+
+    }
+
+}
